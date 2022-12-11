b2cd728c5e12702dfa76e119558d4a395d9cf06b
Ethan Swan <ethanpswan@gmail.com>
2022-12-11 01:05:16 -0600

    Begin transforming to track factors.

--- 11/python/11b.py
+++ 11/python/11b.py
@@ -1,36 +1,68 @@
 import sys
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Callable
+from enum import Enum
+from math import floor
+
+class OperationType(Enum):
+    addition = 1
+    multiplication = 2
+    square = 3
+
 
 @dataclass
 class Item:
     priority: int
-    path: list[int]
+    factors: set[int]
+
+    @classmethod
+    def from_string(cls, s: str) -> 'Item':
+        i = int(s)
+        factors = set()
+        for factor in range(2, floor(i ** 0.5) + 1):
+            if (i % factor) == 0:
+                factors.add(factor)
+        return cls(i, factors)
+
 
 @dataclass
 class Monkey:
     number: int  # What number monkey is this?
-    items: list[int]  # Items the monkey is holding.
-    operation: str  # Store the operation as a string to be `eval`ed later.
+    items: list[Item]  # Items the monkey is holding.
+    operation_type: OperationType  # Do we add, multiply, square, etc
+    operation_args: tuple[int, ...]  # What arguments to use in the operation
     divisor: int  # The number to divide by as part of our "test"
     on_true: int  # What number monkey to throw an item to if the test is true.
     on_false: int  # What number monkey to throw an item to if the test is false.
     _inspections: int = 0  # How many items has this monkey inspected?
 
-    def inspect_items(self) -> list[tuple[int, int]]:
+    def inspect_items(self) -> list[tuple[Item, int]]:
         to_move = []
         while len(self.items) > 0:
             to_move.append(self.inspect_first())
         return to_move
 
-    def inspect_first(self) -> tuple[int, int]:
+    def inspect_first(self) -> tuple[Item, int]:
         self._inspections += 1
         item = self.items.pop(0)
 
-        item = eval(self.operation, {'old': item})
-        # item = item // 3
+        match self.operation_type:
+            case OperationType.square:
+                item.priority = item.priority * item.priority
+            case OperationType.addition:
+                item.priority = item.priority + self.operation_args[0]
+                item.factors = set()  # Todo -- actually compute new factors
+            case OperationType.multiplication:
+                for multiplier in self.operation_args:
+                    item.priority = item.priority * multiplier
+                    item.factors.add(multiplier)
+            case _:
+                raise RuntimeError('Unexpected operation type')
 
-        if (item % self.divisor) == 0:
+        item.priority = item.priority // 3
+
+        if (item.priority % self.divisor) == 0:
             return (item, self.on_true)
         else:
             return (item, self.on_false)
@@ -42,19 +74,34 @@ class Monkey:
         # Sorry for ugly parsing; should use a regex but it would take longer to set up.
         number = int(lines[0].split(' ')[-1][:-1])
         item_list = lines[1].split(':')[1]
-        items = [int(x) for x in item_list.split(', ')]
-        operation = lines[2].split(' new = ')[1]
+        items = [Item.from_string(x) for x in item_list.split(', ')]
+        operation_string = lines[2].split(': new = ')[1]
+        match operation_string.split(' '):
+            case ['old', '*', 'old']:
+                operation_type = OperationType.square
+                args = ()
+            case ['old', '+', addend]:
+                a = int(addend)
+                operation_type = OperationType.addition
+                args = (a,)
+            case ['old', '*', multiplier]:
+                m = int(multiplier)
+                operation_type = OperationType.multiplication
+                args = (m,)
+            case _:
+                raise RuntimeError('unexpected input')
+
         divisor = int(lines[3].split(' divisible by ')[1])
         on_true = int(lines[4].split(' ')[-1])
         on_false = int(lines[5].split(' ')[-1])
-        return cls(number, items, operation, divisor, on_true, on_false)
+        return cls(number, items, operation_type, args, divisor, on_true, on_false)
  
 
 filename = sys.argv[1]
 sections = Path(filename).read_text().strip().split('\n\n')
 monkeys = [Monkey.from_section(section) for section in sections]
 
-N_ROUNDS = 1000
+N_ROUNDS = 20
 for i in range(N_ROUNDS):
     if i % 100 == 0:
         print(f'Round {i}')
