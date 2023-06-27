304a25e6ef76feea7f3e79f1aefa564d2617808d
Ethan Swan <ethanpswan@gmail.com>
2023-06-26 19:42:27 -0500

    Begin implementing exclusion.

--- 15/rust/src/grid.rs
+++ 15/rust/src/grid.rs
@@ -20,26 +20,53 @@ impl Grid {
     fn beacons(&self) -> HashSet<&Position> {
         self.readings.iter().map(|r| &r.beacon).collect()
     }
+
+    fn min_and_max_coords(&self) -> (Position, Position) {
+        let sensors = self.sensors();
+        let beacons = self.beacons();
+        let excluded = self.excluded_positions();
+        let excluded: HashSet<&Position> = excluded.iter().collect();
+        let all_positions = sensors.iter().chain(beacons.iter()).chain(excluded.iter());
+
+        let (mut min_x, mut max_x, mut min_y, mut max_y) = (0, 0, 0, 0);
+        for p in all_positions {
+            if p.x < min_x {
+                min_x = p.x
+            }
+            if p.x > max_x {
+                max_x = p.x
+            }
+            if p.y < min_y {
+                min_y = p.y
+            }
+            if p.y > max_y {
+                max_y = p.y
+            }
+        }
+        (Position{ x: min_x, y: min_y }, Position{ x: max_x, y: max_y })
+    }
 }
 
 impl Display for Grid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let sensors = self.sensors();
         let beacons = self.beacons();
-        let min_x = sensors.iter().chain(beacons.iter()).map(|p| p.x).min().unwrap();
-        let max_x = sensors.iter().chain(beacons.iter()).map(|p| p.x).max().unwrap();
-        let min_y = sensors.iter().chain(beacons.iter()).map(|p| p.y).min().unwrap();
-        let max_y = sensors.iter().chain(beacons.iter()).map(|p| p.y).max().unwrap();        
-
+        let excluded = self.excluded_positions();
+        let (Position{ x: min_x, y: min_y }, Position{ x: max_x, y: max_y }) = self.min_and_max_coords();
         for y in min_y..=max_y {
             for x in min_x..=max_x {
-                let sensor = sensors.iter().find(|p| p.x == x && p.y == y);
-                let beacon = beacons.iter().find(|p| p.x == x && p.y == y);
-                match (sensor, beacon) {
-                    (Some(_), Some(_)) => write!(f, "X")?,
-                    (Some(_), None) => write!(f, "S")?,
-                    (None, Some(_)) => write!(f, "B")?,
-                    (None, None) => write!(f, ".")?,
+                let position = Position{ x, y };
+                let sensor = sensors.contains(&position);
+                let beacon = beacons.contains(&position);
+                let excluded = excluded.contains(&position);
+                if sensor {
+                    write!(f, "S")?;
+                } else if beacon {
+                    write!(f, "B")?;
+                } else if excluded {
+                    write!(f, "#")?;
+                } else {
+                    write!(f, " ")?;
                 }
             }
             write!(f, "\n")?;
@@ -58,6 +85,10 @@ impl Grid {
         }
         Ok(Grid { readings })
     }
+
+    fn excluded_positions(&self) -> HashSet<Position> {
+        self.readings.iter().map(|r| r.excluded_positions()).flatten().collect()
+    }
 }
 
 #[derive(Debug)]
@@ -73,6 +104,19 @@ impl Reading {
     pub fn new(sensor: Position, beacon: Position) -> Reading {
         let distance = manhattan(&sensor, &beacon);
         Reading { sensor, beacon, distance }
+    }
+
+    // Get all the positions that we can rule out as having a beacon.
+    pub fn excluded_positions(&self) -> HashSet<Position> {
+        let mut excluded = HashSet::new();
+        let x_range = self.sensor.x - self.distance..=self.sensor.x + self.distance;
+        let y_range = self.sensor.y - self.distance..=self.sensor.y + self.distance;
+        for x in x_range {
+            for y in y_range.clone() {
+                excluded.insert(Position { x, y });
+            }
+        }
+        excluded
     }
 }
 
