748b70b8571c6ad9bbdd85f1d55074a1a4a4adb8
Ethan Swan <eswan18@rocketmail.com>
2023-07-02 20:43:55 -0500

    Start writing some code and realize it won't work.

--- 16/rust/src/b.rs
+++ 16/rust/src/b.rs
@@ -1,3 +1,12 @@
-pub fn main(_contents: String) -> Result<String, String> {
-    Ok(String::from("TODO"))
+use crate::parse;
+use crate::valve::DistanceMatrix;
+
+const STEPS: usize = 26;
+
+pub fn main(contents: String) -> Result<String, String> {
+    let valves = parse::parse(contents)?;
+    let distance_matrix = DistanceMatrix::new(valves);
+    let distance_matrix = distance_matrix.with_valves_removed();
+    let max_flow = distance_matrix.maximize_flow_2_paths(STEPS);
+    Ok(max_flow.to_string())
 }
--- 16/rust/src/valve.rs
+++ 16/rust/src/valve.rs
@@ -115,6 +115,12 @@ impl DistanceMatrix {
         all_flows.last().unwrap().flow
     }
 
+    pub fn maximize_flow_2_paths(&self, max_steps: usize) -> usize {
+        let mut all_flows = self.all_flows_2_paths(Path{valves: vec![], flow: 0}, Path{valves: vec![], flow: 0}, 0, max_steps);
+        all_flows.sort_by(|p1, p2| p1.flow.cmp(&p2.flow));
+        all_flows.last().unwrap().flow
+    }
+
     fn all_flows(&self, path: Path, steps_taken: usize, max_steps: usize) -> Vec<Path> {
         if path.valves.len() == self.valves.len() {
             println!("Encountered all valves. Finished path {:?} with flow {}", path.valves, path.flow);
@@ -151,6 +157,93 @@ impl DistanceMatrix {
             })
             .flatten()
             .collect() 
+    }
+
+    fn all_flows_2_paths(&self, path_1: Path, path_2: Path, steps_taken: usize, max_steps: usize) -> Vec<(Path, Path)> {
+        if path_1.valves.len() + path_2.valves.len() == self.valves.len() {
+            println!("Encountered all valves. Finished paths {:?} with flow {}", (path_1.valves, path_2.valves), path_1.flow + path_2.flow);
+            return vec![(path_1, path_2)];
+        }
+        if steps_taken >= max_steps {
+            println!("Ran out of steps. Finished paths {:?} with flow {}", (path_1.valves, path_2.valves), path_1.flow + path_2.flow);
+            return vec![(path_1, path_2)];
+        }
+        let at_1 = path_1.valves.last().map(|s| s.as_str()).unwrap_or(STARTING_VALVE);
+        let at_2 = path_2.valves.last().map(|s| s.as_str()).unwrap_or(STARTING_VALVE);
+
+        let potential_steps_1: Vec<(String, usize)> = self
+            .paths_from(&at_1)
+            .into_iter()
+            .filter(|(name, _)| !path_1.valves.contains(name) && !path_2.valves.contains(name))
+            .collect();
+        let potential_steps_2: Vec<(String, usize)> = self
+            .paths_from(&at_2)
+            .into_iter()
+            .filter(|(name, _)| !path_1.valves.contains(name) && !path_2.valves.contains(name))
+            .collect();
+        if potential_steps_1.len() == 0 && potential_steps_2.len() == 0 {
+            println!("Hit dead end. Finished paths {:?} with flow {}", (path_1.valves, path_2.valves), path_1.flow + path_2.flow);
+            return vec![(path_1, path_2)];
+        }
+        if potential_steps_1.len() == 0 {
+            // If player 1 is stuck, only move player 2.
+            return potential_steps_2
+                .into_iter()
+                .map(|(destination, distance)| {
+                    let mut seen = path_2.valves.clone();
+                    seen.push(destination.clone());
+                    // Account for both the distance traveled and the time spent turning on the valve.
+                    let steps_taken = steps_taken + distance + 1;
+                    if steps_taken > max_steps {
+                        return vec![(path_1.clone(), Path{valves: seen, flow: path.flow})];
+                    }
+                    let steps_remaining = max_steps - steps_taken;
+                    let flow = path_2.flow + self.flow_at(&destination) * steps_remaining;
+                    let new_path = Path{valves: seen, flow};
+                    self.all_flows_2_paths(path_1.clone(), new_path, steps_taken, max_steps)
+                })
+                .flatten()
+                .collect() 
+        } else if potential_steps_2.len() == 0 {
+            // If player 2 is stuck, only move player 1.
+            return potential_steps_1
+                .into_iter()
+                .map(|(destination, distance)| {
+                    let mut seen = path_1.valves.clone();
+                    seen.push(destination.clone());
+                    // Account for both the distance traveled and the time spent turning on the valve.
+                    let steps_taken = steps_taken + distance + 1;
+                    if steps_taken > max_steps {
+                        return vec![(Path{valves: seen, flow: path.flow}, path_2.clone())];
+                    }
+                    let steps_remaining = max_steps - steps_taken;
+                    let flow = path_1.flow + self.flow_at(&destination) * steps_remaining;
+                    let new_path = Path{valves: seen, flow};
+                    self.all_flows_2_paths(new_path, path_2.clone(), steps_taken, max_steps)
+                })
+                .flatten()
+                .collect();
+        } 
+        // If there are potential steps for both players, find all combinations of them.
+        for (destination_1, distance_1) in potential_steps_1 {
+            for (destination_2, distance_2) in potential_steps_2 {
+                let mut seen_1 = path_1.valves.clone();
+                seen_1.push(step_1.0.clone());
+                let mut seen_2 = path_2.valves.clone();
+                seen_2.push(step_2.0.clone());
+                // Account for both the distance traveled and the time spent turning on the valve.
+                let steps_taken = steps_taken + step_1.1 + step_2.1 + 1;
+                if steps_taken > max_steps {
+                    return vec![(Path{valves: seen_1, flow: path_1.flow}, Path{valves: seen_2, flow: path_2.flow})];
+                }
+                let steps_remaining = max_steps - steps_taken;
+                let flow_1 = path_1.flow + self.flow_at(&step_1.0) * steps_remaining;
+                let flow_2 = path_2.flow + self.flow_at(&step_2.0) * steps_remaining;
+                let new_path_1 = Path{valves: seen_1, flow: flow_1};
+                let new_path_2 = Path{valves: seen_2, flow: flow_2};
+                return self.all_flows_2_paths(new_path_1, new_path_2, steps_taken, max_steps);
+            }
+        }
     }
 
     fn flow_at(&self, name: &str) -> usize {
