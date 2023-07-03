135c9e71bc8327de4ad762f2e0d37375ff514176
Ethan Swan <eswan18@rocketmail.com>
2023-07-02 19:01:30 -0500

    Nice-ify some things.

--- 16/rust/src/a.rs
+++ 16/rust/src/a.rs
@@ -1,10 +1,11 @@
 use crate::parse;
-use crate::valve::{Valve, self};
+use crate::valve::{Valve, DistanceMatrix};
 
 pub fn main(contents: String) -> Result<String, String> {
     let valves = parse::parse(contents)?;
-    let distance_matrix = valve::compute_distance_matrix(&valves);
-    println!("{:?}", distance_matrix);
-    println!("AA->BB {:?}", distance_matrix.get("AA").unwrap().get("BB").unwrap());
+    let distance_matrix = DistanceMatrix::new(valves);
+    let distance_matrix = distance_matrix.with_valves_removed();
+    println!("{}", distance_matrix);
+    println!("AA->BB {:?}", distance_matrix.distance("AA", "BB"));
     Ok(String::from("TODO"))
 }
