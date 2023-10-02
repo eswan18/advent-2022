70c13f8650e79294ee11a5f404a55c43a9d33413
Ethan Swan <ethanpswan@gmail.com>
2023-10-01 20:57:14 -0400

    Add 01b in scala.

--- 01/scala/src/main/scala/Main.scala
+++ 01/scala/src/main/scala/Main.scala
@@ -4,17 +4,20 @@ import scala.io.Source
 import scala.util.{Try, Success, Failure, Using}
 
 @main def main(args: String*): Unit = 
-  if args.length != 1 then
+  if args.length != 2 then
     println("Usage: sbt run [name]")
-  
-  val inputFile = args(0)
-  val result = run(inputFile)
+  val problem = args(0)
+  val inputFile = args(1)
+  val result = problem match
+    case "a" => run_a(inputFile)
+    case "b" => run_b(inputFile)
+    case _ => Failure(new Exception(s"Unknown problem: $problem"))
   result match
     case Success(s) => println(s"Result: $s")
     case Failure(ex) => println(s"Error: ${ex.getMessage}")
 
 
-def run(inputFile: String): Try[Integer] = 
+def run_a(inputFile: String): Try[Integer] = 
   val lines = linesOfFile(inputFile) match
     case Success(lines) => lines
     case Failure(ex) => return Failure(ex)
@@ -28,6 +31,23 @@ def run(inputFile: String): Try[Integer] =
   val sums = groups.map(_.map(_.toInt).sum)
   Try { sums.max }
 
+def run_b(inputFile: String): Try[Integer] =
+  val lines = linesOfFile(inputFile) match
+    case Success(lines) => lines
+    case Failure(ex) => return Failure(ex)
+  // group the lines, breaking when a blank line is found
+  val groups = lines.foldLeft(List(List[String]())) { (acc, line) =>
+    if line.isEmpty then
+      List[String]() :: acc
+    else
+      (line :: acc.head) :: acc.tail
+  }
+  // Order the groups by decreasing sum
+  val orderedGroups = groups.sortBy(_.map(_.toInt).sum).reverse
+  // Get the total of the first three groups
+  Try {
+    orderedGroups.take(3).map(_.map(_.toInt).sum).sum
+  }
 
 def linesOfFile(filename: String): Try[List[String]] =
   Try {
